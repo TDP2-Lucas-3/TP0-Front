0d80ff0f54036f7cd6974e56f902b788c081800d
azowyl <facugerstner_29@hotmail.com>
2020-09-30 16:59:42 -0300

    add temperature

--- app/src/main/java/com/example/weather/fragments/WeatherDisplayFragment.kt
+++ app/src/main/java/com/example/weather/fragments/WeatherDisplayFragment.kt
@@ -7,10 +7,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.example.weather.R
 import com.example.weather.viewmodels.ForecastViewModel
-import kotlinx.android.synthetic.main.activity_main.*
 
 class WeatherDisplayFragment() : Fragment() {
     private lateinit var viewModel: ForecastViewModel;
@@ -23,7 +21,10 @@ class WeatherDisplayFragment() : Fragment() {
 
         viewModel = ViewModelProvider(this).get(ForecastViewModel::class.java)
         val forecast = viewModel.fetchForecast()
-        activity?.findViewById<TextView>(R.id.weatherText)?.text = forecast?.weather;
-        return inflater.inflate(R.layout.weather_display, container, false)
+
+        val view = inflater.inflate(R.layout.weather_display, container, false)
+        view.findViewById<TextView>(R.id.weatherText)?.text = forecast?.temperatureAsCelsiusStr()
+
+        return view
     }
 }
--- app/src/main/java/com/example/weather/models/Forecast.kt
+++ app/src/main/java/com/example/weather/models/Forecast.kt
@@ -1,4 +1,8 @@
 package com.example.weather.models
 
 class Forecast(val weather: String, val temperature: Float) {
+
+    fun temperatureAsCelsiusStr(): String {
+        return "${temperature.toInt()} ºC"
     }
+}
