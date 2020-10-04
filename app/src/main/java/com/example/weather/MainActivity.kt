c48324be1e026a0f6c9aa20ab373a496ae147345
matiasReimondo <matiasreimondo@gmail.com>
2020-10-04 14:00:22 -0300

    Cambio en la barra

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.weather
 
+
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weather.support.RainCheckAlarmSetup
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         RainCheckAlarmSetup().setup(this)
         setContentView(R.layout.activity_main)
 
+
     }
+
+
+
 }
