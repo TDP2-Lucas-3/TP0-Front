6561a5f976787998ad617c57d7cba24b0ea6c14a
Azowyl <facugerstner_29@hotmail.com>
2020-10-04 12:02:17 -0300

    add background change logic

--- app/src/main/java/com/example/weather/WeatherDefaults.kt
+++ app/src/main/java/com/example/weather/WeatherDefaults.kt
@@ -5,5 +5,6 @@ class WeatherDefaults {
     companion object {
         const val RAIN_THRESHOLD = 90
         const val ACTION_PERIODIC_RAIN_CHECK = "PERIODIC_RAIN_CHECK"
+        const val COLD_THRESHOLD = 15
     }
 }
--- app/src/main/java/com/example/weather/fragments/BackgroundFragment.kt
+++ app/src/main/java/com/example/weather/fragments/BackgroundFragment.kt
@@ -6,11 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.weather.R
+import com.example.weather.WeatherDefaults
 import com.example.weather.models.Forecast
 
 class BackgroundFragment : ForecastFragment() {
+    private fun updateBackground(forecast: Forecast) {
+        val newBackground = when {
+            forecast.rain > WeatherDefaults.RAIN_THRESHOLD -> R.drawable.rain
+            forecast.temp < WeatherDefaults.COLD_THRESHOLD -> R.drawable.cold
+            else -> R.drawable.sunny
+        }
+        activity?.findViewById<ConstraintLayout>(R.id.container_layout)?.setBackgroundResource(newBackground)
+    }
+
     override fun updateScreen(forecast: Forecast) {
-        activity?.findViewById<ConstraintLayout>(R.id.container_layout)?.setBackgroundResource(R.drawable.rain)
+        updateBackground(forecast)
     }
 
     override fun onCreateView(
